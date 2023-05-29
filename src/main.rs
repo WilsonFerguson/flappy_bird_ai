f76ea4e6b3a64b519767f5fd4bf90d4a9f3e09fa
BuildTools <unconfigured@null.spigotmc.org>
2023-05-29 12:25:38 -0700

    added pipes

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@ use piston_window::*;
 mod bird;
 use bird::Bird;
 
+mod pipe;
+use pipe::Pipe;
+
+pub static WIDTH: u32 = 1280;
+pub static HEIGHT: u32 = 720;
+
 fn main() {
     let mut birds: Vec<Bird> = Vec::new();
     let num_birds = 100;
@@ -12,7 +18,12 @@ fn main() {
         birds.push(Bird::new());
     }
 
-    let mut window: PistonWindow = WindowSettings::new("Test App", [1280, 720])
+    let mut pipes: Vec<Pipe> = Vec::new();
+    pipes.push(Pipe::new());
+    let pipe_gap = 350.0;
+    let pipe_speed = 4.0;
+
+    let mut window: PistonWindow = WindowSettings::new("Test App", [WIDTH, HEIGHT])
         .exit_on_esc(true)
         .build()
         .unwrap();
@@ -20,8 +31,21 @@ fn main() {
         window.draw_2d(&event, |context, graphics, _device| {
             clear([0.1; 4], graphics);
 
-            birds.iter().for_each(|bird| bird.draw(&context, graphics));
+            if let Some(pipe) = pipes.last() {
+                if pipe.x < WIDTH as f64 - pipe_gap {
+                    pipes.push(Pipe::new());
+                }
+            }
+            for i in (0..pipes.len()).rev() {
+                let should_remove = pipes.get_mut(i).unwrap().update(pipe_speed);
+                if should_remove {
+                    pipes.remove(i);
+                }
+            }
+            pipes.iter().for_each(|pipe| pipe.draw(&context, graphics));
+
             birds.iter_mut().for_each(|bird| bird.update());
+            birds.iter().for_each(|bird| bird.draw(&context, graphics));
         });
     }
 }
--- src/pipe.rs
+++ src/pipe.rs
@@ -1,10 +1,54 @@
+extern crate piston_window;
+use piston_window::*;
+
+use rand::prelude::*;
+
+use crate::{HEIGHT, WIDTH};
+
 pub struct Pipe {
-    x: f64,
+    pub x: f64,
     width: f64,
     top_gap: f64,
     bottom_gap: f64,
 }
 
 impl Pipe {
-    pub fn new() -> Self {}
+    pub fn new() -> Self {
+        let mut rng = thread_rng();
+        let gap_size = rng.gen_range(150..225);
+        let top_gap = rng.gen_range(100..HEIGHT - gap_size - 100) as f64;
+        let bottom_gap = top_gap + gap_size as f64;
+        Pipe {
+            x: WIDTH as f64,
+            width: rng.gen_range(50..100) as f64,
+            top_gap,
+            bottom_gap,
+        }
+    }
+
+    pub fn update(&mut self, speed: f64) -> bool {
+        self.x -= speed;
+
+        return self.x < -self.width;
+    }
+
+    pub fn draw(&self, context: &Context, graphics: &mut G2d) {
+        rectangle(
+            [0.12, 0.6, 0.25, 1.0],
+            [self.x, 0.0, self.width, self.top_gap],
+            context.transform,
+            graphics,
+        );
+        rectangle(
+            [0.12, 0.6, 0.25, 1.0],
+            [
+                self.x,
+                self.bottom_gap,
+                self.width,
+                HEIGHT as f64 - self.bottom_gap,
+            ],
+            context.transform,
+            graphics,
+        );
+    }
 }
